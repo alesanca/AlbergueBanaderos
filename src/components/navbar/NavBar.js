52112ac726222f69faa92f36e86c2daef0e84c64
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-12 12:56:29 +0000

    Navbar added
    Navbar added using bootstrap

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,10 +1,31 @@
 import React , {Component} from 'react';
+import Nav from 'react-bootstrap/Nav'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class NavBar extends Component {
 
     render() {
         return (
-            <h1> Hola </h1>
+            <Nav defaultActiveKey="/home" as="ul">
+                <Nav.Item as="li">
+                    <Nav.Link href="/home">El albergue</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-1">Nuestros animales</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-2">FAQ</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-3">Legislación</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-4">Contacto</Nav.Link>
+                </Nav.Item>
+                <Nav.Item as="li">
+                    <Nav.Link eventKey="link-5">Animales perdidos</Nav.Link>
+                </Nav.Item>
+            </Nav>
         )
     }
 }
