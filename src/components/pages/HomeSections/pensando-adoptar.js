14d19ebcb3fe18b6f9bff0ce998f7363679d5f9a
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-14 12:46:37 +0000

    New section added in home page

--- src/components/pages/HomeSections/pensando-adoptar.js
+++ src/components/pages/HomeSections/pensando-adoptar.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import '../../../assets/css/home.css';
+
 
 const PensandoAdoptar = () => {
     return (
         <div className="pensandoAdoptar">
             <h2> ¿Pensando en adoptar una mascota? </h2>
+            <div className="grid-container">
+            
+                <div className="flex-item">
+                    <p> ¿Por qué es mejor adoptar? </p>
+                    <p>Descubre todos los beneficios de la adopción de mascotas. </p>
+                    <button> Quiero saber más </button>
+                </div>
+
+                <div className="flex-item">
+                    <p> Elige tu mascota </p>
+                    <p>Te ayudamos a encontrar a la mejor mascota para tí y tu familia. </p>
+                    <button> Quiero saber más </button>
+                </div>
+
+                <div className="flex-item">
+                    <p> ¿Tienes alguna duda? </p>
+                    <p> Consulta nuestra sección de preguntas frecuentes. </p>
+                    <button> Quiero saber más </button>
+                </div>
+                
+            </div>
         </div>
     )
 }
