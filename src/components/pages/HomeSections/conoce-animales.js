516c3a325ff6d86582a34463d8186692ea0d419a
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-15 11:57:27 +0000

    CSS and main contect in the new section added

--- src/components/pages/HomeSections/conoce-animales.js
+++ src/components/pages/HomeSections/conoce-animales.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const ConoceAnimales = () => {
     return (
-        <div>
-            <h3> Hola</h3>
+        <div className="conoceAnimales">
+            <h2> Conoce alguno de nuestros animales </h2>
+            <p>
+            En el Albergue Insular de Animales de Gran Canaria queremos dar la misma oportunidad a todos los animales, por ello te mostramos aquí algunos de forma aleatoria, sin distinción. ¡Porque todos ellos se merecen una oportunidad!
+            </p>
+            <h3> Aquí se hace la consulta a la BD </h3>
         </div>
     )
 }
