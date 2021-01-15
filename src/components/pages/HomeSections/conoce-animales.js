d06620ff7287b7de58436d73716c03ba2261c5c6
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-15 11:41:05 +0000

    New section added

---
+++ src/components/pages/HomeSections/conoce-animales.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConoceAnimales = () => {
+    return (
+        <div>
+            <h3> Hola</h3>
+        </div>
+    )
+}
+
+export default ConoceAnimales;
--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PensandoAdoptar from './HomeSections/pensando-adoptar';
+import ConoceAnimales from './HomeSections/conoce-animales';
 
 const Home = () => {
     return (
         <>
         <PensandoAdoptar />
+        <ConoceAnimales />
         </>
     )
 }
