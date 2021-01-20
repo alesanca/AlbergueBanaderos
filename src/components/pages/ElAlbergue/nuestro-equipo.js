cecb80986a305798e4ddb5d8e3a1f400f03440c9
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-20 19:46:58 +0000

    Nuestro equipo page added

---
+++ src/components/pages/ElAlbergue/nuestro-equipo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const nuestroEquipo = () => {
+    return (
+        <div>
+            <h2> Nuestro equipo </h2>
+            <p>El albergue está formado por un equipo humano de 13 personas entre veterinarios, auxiliares veterinarios, auxiliares administrativos y operarios de mantenimiento y limpieza.
+
+Dicho personal atiende los animales que llegan al centro, con el fin de darles las mejores atenciones hasta que encuentren un familia que les de un hogar.
+
+Gracias a su empeño y dedicación, más de 2.000 animales tienen una segunda oportunidad, y un número muy importante son recuperados de heridas y enfermedades que hacían peligrar su vida.</p>
+        </div>
+    )
+}
+
+export default nuestroEquipo;
--- src/components/pages/el-albergue.js
+++ src/components/pages/el-albergue.js
@@ -2,12 +2,14 @@ import React from 'react';
 import  entrada  from '../../assets/images/entrada.jpg';
 import '../../assets/css/elalbergue.css'
 import NuestraMision from './ElAlbergue/nuestra-mision'
+import NuestroEquipo from './ElAlbergue/nuestro-equipo';
 
 const Albergue = () => {
     return (
         <>
         <img src={entrada} alt="entrada"></img>
         <NuestraMision />
+        <NuestroEquipo />
         </>
     )
 }
