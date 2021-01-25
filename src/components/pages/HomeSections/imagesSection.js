09264cba393ddadcd0e6292b38e81a510444691f
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-25 12:03:20 +0000

    Images added

---
+++ src/components/pages/HomeSections/imagesSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import todo from '../../../assets/images/todo.PNG';
+
+const imagesSection = () => {
+    return (
+        <div className="imagesSection">
+            <img src={todo} alt="perro"></img>
+        </div>
+    )
+}
+
+export default imagesSection;
