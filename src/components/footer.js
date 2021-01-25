539cfb6914f598e0e50ef3b33e2181f84189bb9f
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-25 11:53:42 +0000

    Footer section finished

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import '../assets/css/footer.css';
+import logos from '../assets/images/logos.PNG'
 
 const footer = () => {
     return (
-        <div>
-            <h1>Soy el footer </h1>
+        <div className="footer">
+
+            <div className="leftSide">
+            <img src={logos} alt="logos"></img>
+                <div className="contacto">
+                    <p> Albergue Insular de Animales de Gran Canaria <br/>
+                        Carretera General del Norte, 86, Arucas. <br/>
+                        Teléfono: 928 17 04 01
+                    </p>
+                </div>
+
+               
+                <div className="enlaces">
+                    <a href="#">Cookies</a>
+                    <a href="#">Aviso legal</a>
+                    <br/>
+                    <a href="#">Política de privacidad</a>
+                    <a href="#">Preguntas frecuentes</a>
+                </div>
+            </div>
+     
+                
+        
+
         </div>
     )
 }
