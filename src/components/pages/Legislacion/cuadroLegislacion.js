1d7be430bd28211080ea29c93c65031604097c96
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-25 12:34:19 +0000

    links added

--- src/components/pages/Legislacion/cuadroLegislacion.js
+++ src/components/pages/Legislacion/cuadroLegislacion.js
@@ -13,35 +13,35 @@ const cuadroLegislacion = () => {
                     <p> Decreto 30/2018, de 5 de marzo </p>
                     <br/>
                     <p>Regula el Régimen Jurídico de la tenencia de animales potencialmente peligrosos en la Comunidad Autónoma deCanarias.</p>
-                    <button a href="#">Descargar documento</button>
+                    <button a href="https://www.albergueanimalesgrancanaria.com/sites/default/files/2018-06/Decreto_30_2018_Ley_de_tenencia_de_animales_potencialmente_peligrosos_Canarias.pdf">Descargar documento</button>
                 </div>
 
                 <div className="flex-item">
                     <p> Decreto 30/2018, de 5 de marzo </p>
                     <br/>
                     <p>Regula el Régimen Jurídico de la tenencia de animales potencialmente peligrosos en la Comunidad Autónoma deCanarias.</p>
-                    <button a href="#">Descargar documento</button>
+                    <button a href="https://www.albergueanimalesgrancanaria.com/sites/default/files/2018-06/RD_287_2002_Tenencia_APP.pdf">Descargar documento</button>
                 </div>
 
                 <div className="flex-item">
                     <p> Decreto 30/2018, de 5 de marzo </p>
                     <br/>
                     <p>Regula el Régimen Jurídico de la tenencia de animales potencialmente peligrosos en la Comunidad Autónoma deCanarias.</p>
-                    <button a href="#">Descargar documento</button>
+                    <button a href="https://www.albergueanimalesgrancanaria.com/sites/default/files/2018-06/Ley_50_1999_Tenencia_Animales_Potencialmente_Peligrosos.pdf">Descargar documento</button>
                 </div>
 
                 <div className="flex-item">
                     <p> Decreto 30/2018, de 5 de marzo </p>
                     <br/>
                     <p>Regula el Régimen Jurídico de la tenencia de animales potencialmente peligrosos en la Comunidad Autónoma deCanarias.</p>
-                    <button a href="#">Descargar documento</button>
+                    <button a href="https://www.albergueanimalesgrancanaria.com/sites/default/files/2018-06/Decreto%20_17_1955_Proteccion_Animales.pdf">Descargar documento</button>
                 </div>
 
                 <div className="flex-item">
                     <p> Decreto 30/2018, de 5 de marzo </p>
                     <br/>
                     <p>Regula el Régimen Jurídico de la tenencia de animales potencialmente peligrosos en la Comunidad Autónoma deCanarias.</p>
-                    <button a href="#">Descargar documento</button>
+                    <button a href="https://www.albergueanimalesgrancanaria.com/sites/default/files/2018-06/Ley_8_1991_Proteccion_de_los_Animales.pdf">Descargar documento</button>
                 </div>
 
             </div>
