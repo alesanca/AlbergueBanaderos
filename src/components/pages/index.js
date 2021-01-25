bd335a1214a4cd30340b5a104107602a6a0f9e3c
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-25 12:03:27 +0000

    Homepage finished

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PensandoAdoptar from './HomeSections/pensando-adoptar';
 import ConoceAnimales from './HomeSections/conoce-animales';
 import ProximoAmigo from './HomeSections/proximoAmigo';
+import ImagesSection from './HomeSections/imagesSection';
 
 const Home = () => {
     return (
         <>
+        <ImagesSection/>
         <PensandoAdoptar />
         <ConoceAnimales />
         <ProximoAmigo />
