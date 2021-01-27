acfdb2de3699d74a9d28c9fde24d5fbc5601ffd6
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-27 13:20:19 +0000

    Cosas

--- app/routes/animals.routes.js
+++ app/routes/animals.routes.js
@@ -3,8 +3,6 @@ module.exports = app => {
   
     var router = require("express").Router();
 
-
-    // Retrieve all Tutorials
     router.get("/", animals.findAll);
   
     app.use('/api/animals', router);
--- src/components/pages/HomeSections/conoce-animales.js
+++ src/components/pages/HomeSections/conoce-animales.js
@@ -8,10 +8,63 @@ const ConoceAnimales = () => {
             <p>
             En el Albergue Insular de Animales de Gran Canaria queremos dar la misma oportunidad a todos los animales, por ello te mostramos aquí algunos de forma aleatoria, sin distinción. ¡Porque todos ellos se merecen una oportunidad!
             </p>
-            <h3> Aquí se hace la consulta a la BD </h3>
+           <MyComponent />
             <button className="MC_btn"> Ver todos los animales </button>
         </div>
     )
 }
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        animals: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("http://localhost:8080/api/animals" , {
+          method: 'GET',
+          mode: 'no-cors'
+      })
+        .then(res => res.json() )
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              animals: result.animals
+            } );
+           
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, animals } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <ul>
+            {animals.map(item => (
+              <li key={item.id}>
+                {item.nombre}
+              </li>
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
 
 export default ConoceAnimales;
