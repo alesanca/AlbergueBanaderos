c12dd4fa90e919686408076bf96a572c4246ade6
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-01-29 12:20:32 +0000

    Chip component created

--- src/components/pages/animales-perdidos.js
+++ src/components/pages/animales-perdidos.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import perrete from '../../assets/images/animalesPerdidos.jpg';
+import '../../assets/css/animalesPerdidos.css'
 
 const AnimalesPerdidos = () => {
     return (
-        <div>
+        <div className="animalesPerdidos">
             <h1> Hola soy un animal perdido </h1>
+            <div className="animalesSection">
+                <Chip />
+            </div>
         </div>
     )
 }
 
 export default AnimalesPerdidos;
+
+const Chip = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
