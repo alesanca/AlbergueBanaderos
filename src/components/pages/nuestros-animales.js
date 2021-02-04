05fa69e7ff31327aefaeab00c44be0cca1d4fafa
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-02-04 19:40:59 +0000

    filter added

--- src/components/pages/nuestros-animales.js
+++ src/components/pages/nuestros-animales.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NuestrosAnimales = () => {
     return (
-        <div>
+        <div className="listSection">
             <AnimalList />
         </div>
     )
@@ -14,21 +14,27 @@ class AnimalList extends React.Component {
       this.state = {
         error: null,
         isLoaded: false,
-        animalList: []
+        animals: []
       };
     }
   
     componentDidMount(type) {
-      fetch("http://localhost:8080/api/animals" , {
-          method: 'GET'
-      })
+      fetch("http://localhost:8080/api/animals")
         .then(res => res.json())
         .then( 
           (result) => {
-            const res = result.filter(animal => animal.tipo == type);
+              
+              if(type){
+                return this.setState({
+                    isLoaded: true,
+                    animals : result
+                  })
+              }
+
+            const animals = result.filter(animal => animal.tipo == type);
             this.setState({
               isLoaded: true,
-              animalList: res
+              animals
             }
             );
           },
@@ -42,7 +48,7 @@ class AnimalList extends React.Component {
     }
   
     render() {
-      const { error, isLoaded, animalList } = this.state;
+      const { error, isLoaded, animals } = this.state;
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
@@ -50,7 +56,7 @@ class AnimalList extends React.Component {
       } else {
         return (
           <div className="escaparate">
-            {animalList.map(item => (
+            {animals.map(item => (
               <div key={item.id} className="individual">
                 <img src={item.url} alt="foto del animal"></img>
                 <h2>{item.nombre}</h2>
