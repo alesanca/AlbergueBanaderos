8b5725346459c2539c79b9d2709cba485423785a
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-02-04 12:30:22 +0000

    AnimalesPerdidos section added

--- src/components/pages/animales-perdidos.js
+++ src/components/pages/animales-perdidos.js
@@ -18,8 +18,13 @@ export default AnimalesPerdidos;
 const Chip = () => {
 
     return (
-        <div>
-            
+        <div className="animalesSection">
+            <h2> Animales Perdidos</h2>
+            <p>Si has perdido a tu perro o gato, puedes consultar si ha sido encontrado y entregado en el Albergue Insular de Animales, para ello debes introducir el número de microchip en la siguiente casilla:</p>
+            <h3>MICROCHIP</h3>
+            <input type="text"></input><br/>
+            <button>BUSCAR ANIMAL</button>
+            <p>Si no tiene microchip instalado y es entregado en el albergue, pasará a estar disponible para su adopción, puedes comprobar en la sección Nuestros animales si se encuentra en el albergue. En caso positivo, para recuperarlo deberás acudir al albergue (no es necesaria cita previa) con alguna prueba fotográfica que demuestre que eres su legítimo dueño.</p>
         </div>
     )
 }
