1e682a624db29801ca111f75087f752989ea88eb
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-02-04 19:01:41 +0000

    Animals List is filter in the frontend now

--- src/components/pages/nuestros-animales.js
+++ src/components/pages/nuestros-animales.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from 'react';
 
 const NuestrosAnimales = () => {
     return (
         <div>
-            <h1> Nuestros Animales </h1>
+            <AnimalList />
         </div>
     )
 }
+
+class AnimalList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        animalList: []
+      };
+    }
+  
+    componentDidMount(type) {
+      fetch("http://localhost:8080/api/animals" , {
+          method: 'GET'
+      })
+        .then(res => res.json())
+        .then( 
+          (result) => {
+            const res = result.filter(animal => animal.tipo == type);
+            this.setState({
+              isLoaded: true,
+              animalList: res
+            }
+            );
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+  
+    render() {
+      const { error, isLoaded, animalList } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+          <div className="escaparate">
+            {animalList.map(item => (
+              <div key={item.id} className="individual">
+                <img src={item.url} alt="foto del animal"></img>
+                <h2>{item.nombre}</h2>
+              </div>
+            ))}
+          </div>
+        );
+      }
+    }
+  }
+
 
 export default NuestrosAnimales;
