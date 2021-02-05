7203ca38bb11f64387e50d5ac39dd89a431ef4f5
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-02-05 12:44:55 +0000

    Filter by animal type added correctly
    De locos

--- src/components/pages/HomeSections/proximoAmigo.js
+++ src/components/pages/HomeSections/proximoAmigo.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Redirect } from "react-router";
 
 const ProximoAmigo = () => {
+  return (
+    <div className="nextFriend">
+      <h1> Tu próximo amigo está esperándote </h1>
+      <Adopta />
+    </div>
+  );
+};
+
+class Adopta extends React.Component {
+  state = {
+    redirect: false,
+  };
+  setRedirect = () => {
+    this.setState({
+      redirect: true,
+    });
+  };
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to={{
+        pathname: "/nuestros-animales",
+        search : "?type=Perro"
+       }}/>;
+    }
+  };
+  render() {
     return (
-        <div>
-            <h1> Hey </h1>
-        </div>
-    )
+      <>
+        {this.renderRedirect()}
+        <button onClick={this.setRedirect}>Adopta un perro</button>
+        
+      </>
+    );
+  }
 }
 
 export default ProximoAmigo;
--- src/components/pages/nuestros-animales.js
+++ src/components/pages/nuestros-animales.js
@@ -1,72 +1,46 @@
-import React from 'react';
+import React from "react";
+import "../../assets/css/nuestrosAnimales.css";
 
-const NuestrosAnimales = () => {
-    return (
-        <div className="listSection">
-            <AnimalList />
-        </div>
-    )
-}
+const NuestrosAnimales = () => <AnimalList />;
 
 class AnimalList extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        animals: []
-      };
-    }
-  
-    componentDidMount(type) {
-      fetch("http://localhost:8080/api/animals")
-        .then(res => res.json())
-        .then( 
-          (result) => {
-              
-              if(type){
-                return this.setState({
-                    isLoaded: true,
-                    animals : result
-                  })
-              }
-
-            const animals = result.filter(animal => animal.tipo == type);
-            this.setState({
-              isLoaded: true,
-              animals
-            }
-            );
-          },
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
-    }
-  
-    render() {
-      const { error, isLoaded, animals } = this.state;
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <div className="escaparate">
-            {animals.map(item => (
-              <div key={item.id} className="individual">
-                <img src={item.url} alt="foto del animal"></img>
-                <h2>{item.nombre}</h2>
-              </div>
-            ))}
-          </div>
-        );
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      animals: [],
+    };
   }
 
+  componentDidMount() {
+    let params = new URLSearchParams(window.location.search);
+    let type = params.get("type");
+    console.log(type);
+    fetch("http://localhost:8080/api/animals")
+      .then((res) => res.json())
+      .then((result) => {
+        let animals = result;
+
+        if (type) animals = result.filter((animal) => animal.tipo === type);
+
+        this.setState({
+          animals,
+        });
+      });
+  }
+
+  render() {
+    console.log(this.state.animals);
+    return (
+      <div className="escaparate">
+        {this.state.animals.map((a) => (
+          <div key={a.id} className="individual">
+            <img src={a.url} alt="foto del animal"></img>
+            <h2>{a.nombre}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default NuestrosAnimales;
