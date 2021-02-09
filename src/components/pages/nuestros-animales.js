1e82b010a18c746fdbd8a4a5473c1144246286b0
Alejandro Sánchez <alesan.ca3@gmail.com>
2021-02-09 19:53:15 +0000

    New feature added

--- src/components/pages/nuestros-animales.js
+++ src/components/pages/nuestros-animales.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "../../assets/css/nuestrosAnimales.css";
 
-const NuestrosAnimales = () => 
-<>
-  <Menu />
-  <AnimalList />
-</>;
-
-class AnimalList extends React.Component {
+export class Animals extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       animals: [],
+      filteredAnimals: [],
     };
+  }
+
+  handleApplyFilter(type) {
+    this.setState({
+      filteredAnimals: this.state.animals.filter(
+        (animal) => animal.tipo === type
+      ),
+    });
   }
 
   componentDidMount() {
@@ -21,36 +24,42 @@ class AnimalList extends React.Component {
     fetch("http://localhost:8080/api/animals")
       .then((res) => res.json())
       .then((result) => {
+        let filteredAnimals = result;
         let animals = result;
 
-        if (type) animals = result.filter((animal) => animal.tipo === type);
+        if (type)
+          filteredAnimals = animals.filter((animal) => animal.tipo === type);
 
         this.setState({
           animals,
+          filteredAnimals,
         });
       });
   }
 
   render() {
     return (
-      <div className="escaparate">
-        {this.state.animals.map((a) => (
-          <div key={a.id} className="individual">
-            <img src={a.url} alt="foto del animal"></img>
-            <h2>{a.nombre}</h2>
-          </div>
-        ))}
-      </div>
+      <>
+        <div className="menuForm">
+          <h3>Filtrar por:</h3>
+          <p>Tipo animal</p>
+          <button onClick={() => this.handleApplyFilter("Perro")}>
+            Adopta un perro
+          </button>
+          <button onClick={() => this.handleApplyFilter("Gato")}>
+            Adopta un gato
+          </button>
+        </div>
+        <div className="escaparate">
+          {this.state.filteredAnimals.map((a) => (
+            <div key={a.id} className="individual">
+              <img src={a.url} alt="foto del animal"></img>
+              <h2>{a.nombre}</h2>
+            </div>
+          ))}
+        </div>
+      </>
     );
   }
 }
 
-const Menu = () => {
-  return (
-    <div className="menuForm">
-      <h3>Filtrar por:</h3>
-    </div>
-  )
-}
-
-export default NuestrosAnimales;
